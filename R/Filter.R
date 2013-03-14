54c5dfd0ee49f5db62ee823618305a4dbfa5ec41
fribalet <ribalet@uw.edu>
2013-03-14 11:30:49 -0700

    new 'filter' function

--- R/Filter.R
+++ R/Filter.R
@@ -9,34 +9,50 @@ filter <- function(events, width=1, notch=1, slope=NULL, edge=1, do.plot=FALSE){
   		detected <- subset(events, D1 > 2^4 & D2 > 2^4) # filtering particles not detected by D1 or D2
   		unsaturated <- subset(detected, D1 < max(events[,"D1"]) & D2 < max(events[,"D1"])) # filtering particles with saturated signals on D1 or D2
   		inside.stream <- subset(unsaturated,D2 < -D1 + edge*2^16*2) # filtering particles at the boundary layer between water/air
+		
 		if(is.null(slope)){
-		  quart1 <- subset(inside.stream, D1 > as.numeric(quantile(inside.stream[,"D1"], probs= 0.9)))
-		  quart2 <- subset(inside.stream, D2 > as.numeric(quantile(inside.stream[,"D2"], probs= 0.9)))
-		  slope <- mean(quart2[,"D2"])/mean(quart1[,"D1"]) # the correction factor for the sensitivity of D1 with respect to D2.			
-		}  
-  		aligned <- subset(inside.stream, D2 > (slope*D1 - width * 10^4) & D2 < (slope*D1 + width*10^4)) # filtering aligned particles (D1 = D2)
+		  # quart1 <- subset(inside.stream, D1 > as.numeric(quantile(inside.stream[,"D1"], probs= 0.9)))
+		  # quart2 <- subset(inside.stream, D2 > as.numeric(quantile(inside.stream[,"D2"], probs= 0.9)))
+		  # slope <- mean(quart2[,"D2"])/mean(quart1[,"D1"]) # the correction factor for the sensitivity of D1 with respect to D2.			
+		  slope.inside.stream <- subset(inside.stream, D1 > 10000 & D2 > 10000) # Exclude potenital electrical noise from calculation.	
+		  slope <- mean(slope.inside.stream$D2/slope.inside.stream$D1) 		
+			}  
+  		a
+  		ligned <- subset(inside.stream, D2 > (slope*D1 - width * 10^4) & D2 < (slope*D1 + width*10^4)) # filtering aligned particles (D1 = D2)
   		focused <- subset(aligned, D2/fsc_small > (slope*D1/fsc_small - notch) & D2/fsc_small < (slope*D1/fsc_small + notch))# filtering focused particles (D1/fsc_small = D2/fsc_small)
   		filtered <- subset(focused, D1/fsc_small < notch/slope | D2/fsc_small < notch*slope) # filtering focused particles (D/fsc_small < notch)
   
   ########################
   
   if(do.plot){
-	par(mfrow=c(2,2))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-
-   plot(events$D1, events$D2, xlab="D1", ylab="D2", 
+	percent.opp <- round(100*nrow(filtered)/nrow(events))
+	
+	par(mfrow=c(2,2),pty='s')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+	
+	events. <- events[1:round(nrow(filtered)),]
+    plot(events.$D1, events.$D2, xlab="D1", ylab="D2", 
     		pch=20, cex=0.25, xlim=c(0,2^16), ylim=c(0,2^16),
-			col= densCols(events$D1, events$D2, colramp=.rainbow.cols),
-			main="Filtering Aligned Particles \n from Total Events", pty='s')
+			col= densCols(events.$D1, events.$D2, colramp=.rainbow.cols),
+			main="Filtering Aligned Particles")
+	 mtext(paste("only", percent.opp,"% display"), side=3, cex=0.7,col='red')
 	 abline(v=max(events[,"D1"]), h=max(events[,"D2"]), col='red',lwd=2)
 	 abline(v=2^4, h=2^4, col='red',lwd=2)
 	 abline(b=slope, a=width*10^4, col='red',lwd=2)
 	 abline(b=slope, a=-width*10^4, col='red',lwd=2)
 	 abline(b=-1, a=edge*2^16*2, col='red',lwd=2)
+	
+	mtext(paste("D1/D2=", round(slope,2)),outer=T,side=3, line=-2,font=2)
+	mtext(paste("Width=", width),outer=T,side=3, line=-3,font=2)
+	mtext(paste("Notch=", notch),outer=T,side=3, line=-4,font=2)
 
-	aligned. <- subset(aligned, aligned$D1/aligned$fsc_small<2 & aligned$D2/aligned$fsc_small<2) 
-    plot(x = aligned.$D1/aligned.$fsc_small,  y = aligned.$D2/aligned.$fsc_small, cex=.25, xlim=c(0,2), ylim=c(0,2),
+	aligned. <- subset(aligned, aligned$D1/aligned$fsc_small<2 & aligned$D2/aligned$fsc_small<2)
+	aligned. <- aligned.[1:round(nrow(filtered)),]
+    para1 <- aligned.$D1/aligned.$fsc_small
+    para2 <- aligned.$D2/aligned.$fsc_small
+    plot(x = para1,  y = para2, cex=.25, xlim=c(0,2), ylim=c(0,2),
          xlab='D1/fsc_small', ylab='D2/fsc_small', col = densCols(aligned.$D1/aligned.$fsc_small, aligned.$D2/aligned.$fsc_small,colramp=.rainbow.cols), pch=20, 
-         main = 'Filtering Focused Particles \n from Aligned Particles',pty='s')    
+         main = 'Filtering Focused Particles',pty='s')    
+	 mtext(paste("only", percent.opp,"% display"), side=3, cex=0.7,col='red')
      abline(v=notch/slope, h=notch*slope, col='red',lwd=2)
 	 abline(b=slope, a=notch, col='red', lwd=2)
 	 abline(b=slope, a=-notch, col='red', lwd=2)
@@ -44,16 +60,23 @@ filter <- function(events, width=1, notch=1, slope=NULL, edge=1, do.plot=FALSE){
    plot(filtered$fsc_small, filtered$pe, xlab="fsc_small", ylab="pe", 
     		pch=20, cex=0.25, xlim=c(0,2^16), ylim=c(0,2^16),
 			col= densCols(filtered$fsc_small, filtered$pe, colramp=.rainbow.cols),
+<<<<<<< HEAD
 			main="Optimally Positioned Particles", pty='s')
    plot(filtered$fsc_small, filtered$chl_small, xlab="fsc_small", ylab="chl_small", 
     		pch=20, cex=0.25, xlim=c(0,2^16), ylim=c(0,2^16),
 			col= densCols(filtered$fsc_small, filtered$chl_small, colramp=.rainbow.cols),
 			main="Optimally Positioned Particles", pty='s')
+=======
+			main=paste("Optimally Positioned Particles\n (",percent.opp,"% total events)"))
+   plot(filtered$fsc_small, filtered$chl_small, xlab="fsc_small", ylab="chl_small", 
+    		pch=20, cex=0.25, xlim=c(0,2^16), ylim=c(0,2^16),
+			col= densCols(filtered$fsc_small, filtered$chl_small, colramp=.rainbow.cols),
+			main=paste("Optimally Positioned Particles\n (",percent.opp,"% total events)"))
+>>>>>>> new 'filter' function
 
   }
   
-  filtered$D1dFSC <- filtered$D2dFSC <- NULL
-   
+  
   return(filtered)
   
 }
@@ -89,12 +112,12 @@ filterFile <- function(evt.path, width=1, notch=1, slope=NULL, edge=1, map.margi
   message('writing')
   writeSeaflow(paste(out.evt.file.path,'.opp',sep=''), filter.frame)
 
-    ## now add a map plot so we can know where we are
-    latlong <- .getSDSlatlon(.getSDS(evt.path))
-    if(!is.null(latlong) & !any(sapply(latlong, is.na))){
-      cruise.track <- try(.makeSDSlatlonDF(read.delim(paste(output.path,'/sds.tab',sep=''))), silent=T) 
-      plotLatLongMap(latlong[1], latlong[2], cruise.track, margin=as.numeric(map.margin), pch=3, cex=2, lwd=2, col='red', main=paste("file: ",.getYearDay(evt.path),'/',basename(evt.path), sep=""))
-      }
+    # ## now add a map plot so we can know where we are
+    # latlong <- .getSDSlatlon(.getSDS(evt.path))
+    # if(!is.null(latlong) & !any(sapply(latlong, is.na))){
+      # cruise.track <- try(.makeSDSlatlonDF(read.delim(paste(output.path,'/sds.tab',sep=''))), silent=T) 
+      # plotLatLongMap(latlong[1], latlong[2], cruise.track, margin=as.numeric(map.margin), pch=3, cex=2, lwd=2, col='red', main=paste("file: ",.getYearDay(evt.path),'/',basename(evt.path), sep=""))
+      # }
    dev.off()	  
 
 
